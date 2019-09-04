bd5b958268adb39b0eac003cb008506f7e18c3ae
Sam Ghatan <samuelghatan@outlook.com>
2019-09-04 17:07:21 +0100

    Cleaning

---
+++ Predicting Car Prices (Knn)/Predicting Car Sales (Knn).py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  4 16:47:55 2019
+
+@author: samuelghatan
+"""
+
+# Introduction into dataset
+
+# In the project I'll be working with a dataset that contains various technical information on cars, including: 
+# the motor's displacement, the weight of the car, the miles per gallon, how fast the car accelerates, and more.
+# In total there are 205 cars and 26 features.
+# I will be using this data to predict the cars market value based on some key features.
+
+import pandas as pd
+import numpy as np
+
+pd.options.display.max_columns = 99
+
+cols = ['symboling', 'normalized-losses', 'make', 'fuel-type', 'aspiration', 'num-of-doors', 'body-style', 
+        'drive-wheels', 'engine-location', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'engine-type', 
+        'num-of-cylinders', 'engine-size', 'fuel-system', 'bore', 'stroke', 'compression-rate', 'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price']
+cars = pd.read_csv('imports-85.data', names=cols)
+print(cars.head())
+
+
+# Select only the columns with continuous values from - https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.names
+
+continuous_values_cols = ['normalized-losses', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'bore', 'stroke', 'compression-rate', 'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price']
+numeric_cars = cars[continuous_values_cols]
+numeric_cars.head(5)
+
+
+# Data Cleaning
+
+numeric_cars = numeric_cars.replace('?', np.nan)
+numeric_cars = numeric_cars.astype('float')
+numeric_cars.isnull().sum()
+
+
+# Because `price` is the column we want to predict, let's remove any rows with missing `price` values.
+
+numeric_cars = numeric_cars.dropna(subset=['price'])
+numeric_cars.isnull().sum()
+
+
+# Replace missing values in other columns using column means.
+
+numeric_cars = numeric_cars.fillna(numeric_cars.mean())
+
+
+# Confirm that there's no more missing values.
+
+numeric_cars.isnull().sum()
+
+
+# Normalize all columnns to range from 0 to 1 except the target column.
+
+price_col = numeric_cars['price']
+numeric_cars = (numeric_cars - numeric_cars.min())/(numeric_cars.max() - numeric_cars.min())
+numeric_cars['price'] = price_col
+
+
+# # Univariate model Knn model
+
+# To start with im going to perform analysis using a simple univariate model to help understand the features of the dataset better before moving on to more complex models.
+
+
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error
+
+def knn_train_test(train_col, target_col, df):
+    knn = KNeighborsRegressor()
+    np.random.seed(1)
+        
+    # Randomize order of rows in data frame.
+    shuffled_index = np.random.permutation(df.index)
+    rand_df = df.reindex(shuffled_index)
+
+    # Divide number of rows in half and round.
+    last_train_row = int(len(rand_df) / 2)
+    
+    # Select the first half and set as training set.
+    # Select the second half and set as test set.
+    train_df = rand_df.iloc[0:last_train_row]
+    test_df = rand_df.iloc[last_train_row:]
+    
+    # Fit a KNN model using default k value.
+    knn.fit(train_df[[train_col]], train_df[target_col])
+    
+    # Make predictions using model.
+    predicted_labels = knn.predict(test_df[[train_col]])
+
+    # Calculate and return RMSE.
+    mse = mean_squared_error(test_df[target_col], predicted_labels)
+    rmse = np.sqrt(mse)
+    return rmse
+
+rmse_results = {}
+train_cols = numeric_cars.columns.drop('price')
+
+# For each column (minus `price`), train a model, return RMSE value
+# and add to the dictionary `rmse_results`.
+for col in train_cols:
+    rmse_val = knn_train_test(col, 'price', numeric_cars)
+    rmse_results[col] = rmse_val
+
+# Create a Series object from the dictionary so 
+# we can easily view the results, sort, etc
+rmse_results_series = pd.Series(rmse_results)
+rmse_results_series.sort_values()
+
+# Which K values does the model perform the best with?
+
+# To answer this I updated the function logic to test different values for K.
+
+def knn_train_test(train_col, target_col, df):
+    np.random.seed(1)
+        
+    # Randomize order of rows in data frame.
+    shuffled_index = np.random.permutation(df.index)
+    rand_df = df.reindex(shuffled_index)
+
+    # Divide number of rows in half and round.
+    last_train_row = int(len(rand_df) / 2)
+    
+    # Select the first half and set as training set.
+    # Select the second half and set as test set.
+    train_df = rand_df.iloc[0:last_train_row]
+    test_df = rand_df.iloc[last_train_row:]
+    
+    k_values = [1,3,5,7,9]
+    k_rmses = {}
+    
+    for k in k_values:
+        # Fit model using k nearest neighbors.
+        knn = KNeighborsRegressor(n_neighbors=k)
+        knn.fit(train_df[[train_col]], train_df[target_col])
+
+        # Make predictions using model.
+        predicted_labels = knn.predict(test_df[[train_col]])
+
+        # Calculate and return RMSE.
+        mse = mean_squared_error(test_df[target_col], predicted_labels)
+        rmse = np.sqrt(mse)
+        
+        k_rmses[k] = rmse
+    return k_rmses
+
+k_rmse_results = {}
+
+# For each column (minus `price`), train a model, return RMSE value
+# and add to the dictionary `rmse_results`.
+train_cols = numeric_cars.columns.drop('price')
+for col in train_cols:
+    rmse_val = knn_train_test(col, 'price', numeric_cars)
+    k_rmse_results[col] = rmse_val
+
+k_rmse_results
+
+
+# # Mulitvariate Model (more than one feature)
+
+
+# First I want to find the best feature from the last step by 
+# Compute average RMSE across different`k` values for each feature.
+
+feature_avg_rmse = {}
+for k,v in k_rmse_results.items():
+    avg_rmse = np.mean(list(v.values())) # .values is needed as values are within 2nd dict
+    feature_avg_rmse[k] = avg_rmse
+series_avg_rmse = pd.Series(feature_avg_rmse)
+series_avg_rmse.sort_values()
+
+
+def knn_train_test(train_cols, target_col, df):
+    np.random.seed(1)
+    
+    # Randomize order of rows in data frame.
+    shuffled_index = np.random.permutation(df.index)
+    rand_df = df.reindex(shuffled_index)
+
+    # Divide number of rows in half and round.
+    last_train_row = int(len(rand_df) / 2)
+    
+    # Select the first half and set as training set.
+    # Select the second half and set as test set.
+    train_df = rand_df.iloc[0:last_train_row]
+    test_df = rand_df.iloc[last_train_row:]
+    
+    k_values = [5]
+    k_rmses = {}
+    
+    for k in k_values:
+        # Fit model using k nearest neighbors.
+        knn = KNeighborsRegressor(n_neighbors=k)
+        knn.fit(train_df[train_cols], train_df[target_col])
+
+        # Make predictions using model.
+        predicted_labels = knn.predict(test_df[train_cols])
+
+        # Calculate and return RMSE.
+        mse = mean_squared_error(test_df[target_col], predicted_labels)
+        rmse = np.sqrt(mse)
+        
+        k_rmses[k] = rmse
+    return k_rmses
+
+# Which number of features is the most opitmal?
+    
+k_rmse_results = {}
+
+two_best_features = ['horsepower', 'width']
+rmse_val = knn_train_test(two_best_features, 'price', numeric_cars)
+k_rmse_results["two best features"] = rmse_val
+
+three_best_features = ['horsepower', 'width', 'curb-weight']
+rmse_val = knn_train_test(three_best_features, 'price', numeric_cars)
+k_rmse_results["three best features"] = rmse_val
+
+four_best_features = ['horsepower', 'width', 'curb-weight', 'city-mpg']
+rmse_val = knn_train_test(four_best_features, 'price', numeric_cars)
+k_rmse_results["four best features"] = rmse_val
+
+five_best_features = ['horsepower', 'width', 'curb-weight' , 'city-mpg' , 'highway-mpg']
+rmse_val = knn_train_test(five_best_features, 'price', numeric_cars)
+k_rmse_results["five best features"] = rmse_val
+
+six_best_features = ['horsepower', 'width', 'curb-weight' , 'city-mpg' , 'highway-mpg', 'length']
+rmse_val = knn_train_test(six_best_features, 'price', numeric_cars)
+k_rmse_results["six best features"] = rmse_val
+
+
+k_rmse_results
+
+# Models containing 3,4,5 performed the most optimally.
+
+# Now let's optimize the model that performed the best in the last step. By varying the hyperparameter k
+
+def knn_train_test(train_cols, target_col, df):
+    np.random.seed(1)
+    
+    # Randomize order of rows in data frame.
+    shuffled_index = np.random.permutation(df.index)
+    rand_df = df.reindex(shuffled_index)
+
+    # Divide number of rows in half and round.
+    last_train_row = int(len(rand_df) / 2)
+    
+    # Select the first half and set as training set.
+    # Select the second half and set as test set.
+    train_df = rand_df.iloc[0:last_train_row]
+    test_df = rand_df.iloc[last_train_row:]
+    
+    k_values = range(1,25)
+    k_rmses = {}
+    
+    for k in k_values:
+        # Fit model using k nearest neighbors.
+        knn = KNeighborsRegressor(n_neighbors=k)
+        knn.fit(train_df[train_cols], train_df[target_col])
+
+        # Make predictions using model.
+        predicted_labels = knn.predict(test_df[train_cols])
+
+        # Calculate and return RMSE.
+        mse = mean_squared_error(test_df[target_col], predicted_labels)
+        rmse = np.sqrt(mse)
+        
+        k_rmses[k] = rmse
+    return k_rmses
+
+# Which hyper parameter performs the best in the model?
+
+k_rmse_results = {}
+
+three_best_features = ['horsepower', 'width', 'curb-weight']
+rmse_val = knn_train_test(three_best_features, 'price', numeric_cars)
+k_rmse_results["three best features"] = rmse_val
+
+four_best_features = ['horsepower', 'width', 'curb-weight', 'city-mpg']
+rmse_val = knn_train_test(four_best_features, 'price', numeric_cars)
+k_rmse_results["four best features"] = rmse_val
+
+five_best_features = ['horsepower', 'width', 'curb-weight' , 'city-mpg' , 'highway-mpg']
+rmse_val = knn_train_test(five_best_features, 'price', numeric_cars)
+k_rmse_results["five best features"] = rmse_val
+
+k_rmse_results
+
+# Plot the results as a graph to more easily visulise the pattern.
+
+import matplotlib.pyplot as plt
+
+for k,v in k_rmse_results.items():
+    x = list(v.keys())
+    y = list(v.values())
+    
+    plt.plot(x,y)
+    plt.xlabel('k value')
+    plt.ylabel('RMSE')
+
+
+# It seems that all models have an optimal k-value somewhere below 5, this could be because the classes are so close that 
+# any large enough k number begins to include values from other classes.
